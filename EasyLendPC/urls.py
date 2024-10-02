e03e6c086138de178d6052b973704f35c67b5e4c
bg-miles <piuschukwuebuka40@gmail>
2024-10-02 23:47:44 +0100

    ignore these

--- EasyLendPC/urls.py
+++ EasyLendPC/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("App.urls"))
+    path("App/", include("App.urls")),
+    path('accounts/', include('accounts.urls', namespace="accounts")),
 ]
--- accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate
-
-
-class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=255)
-    password = forms.CharField(widget=forms.PasswordInput)
