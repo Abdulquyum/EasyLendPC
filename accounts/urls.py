2d16767d21a965e9eb818454c77feb331120bb43
bg-miles <piuschukwuebuka40@gmail>
2024-10-02 23:44:33 +0100

    user authentication v2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "accounts"
+
 urlpatterns = [
-    path("dashboard", views.dashboard, name="dashboard"),
-    path("signup", views.signup_view, name="signup"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("signup/", views.signup_view, name="signup"),
     path("login/", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,41 +1,70 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
-from .forms import UserLoginForm
 
 # Create your views here.
 
 
-@login_required(login_url="/accounts/login")
-def dashboard(request):
-    return render(request, "dashboard.html")
+@login_required(login_url="accounts:login")
+def dashboard(user_request):
+    return render(user_request, "dashboard.html")
 
 
-def signup_view(request):
+def login_view(user_request):
+    if user_request.method == "POST":
+        username = user_request.POST["username"]
+        password = user_request.POST["password"]
 
-    return render(request, "signup.html")
+        user = auth.authenticate(username=username, password=password)
 
-
-def login_view(request):
-    if request.method == "POST":
-        form = UserLoginForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, f"Welcome Back {username}")
-                return redirect("dashboard")
-            else:
-                messages.error(request, "Invalid username or password")
+        # check if user is availble in the database
+        if user is not None:
+            auth.login(user_request, user)
+            return redirect("accounts:dashboard")
         else:
-            messages.error(request, "Invalid Form Submission")
+            messages.info(user_request, "Invalid username or password")
+            return redirect("accounts:login")
+    return render(user_request, "login.html")
+
+
+def signup_view(user_request):
+    # Collect user data
+    if user_request.method == "POST":
+        email = user_request.POST["email"]
+        username = user_request.POST["username"]
+        password = user_request.POST["password"]
+        password2 = user_request.POST["password2"]
+
+        if password == password2:
+            # Check if user already exist
+            if User.objects.filter(email=email).exists():
+                messages.info(user_request, "Email taken")
+                return redirect("accounts:signup")
+            elif User.objects.filter(username=username).exists():
+                messages.info(user_request, "Username taken")
+                return redirect("accounts:signup")
+            else:  # Create a new user if it dosen't exist
+                user = User.objects.create_user(
+                    username=username, email=email, password=password
+                )
+                user.save()
+
+                # Log user in
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(user_request, user_login)
+                return redirect("accounts:dashboard")
+
+        else:
+            messages.info(user_request, "Input Matching Passwords")
+            return redirect("accounts:signup")
     else:
-        form = UserLoginForm()
-    return render(request, "login.html", {"form": form})
+        return render(user_request, "signup.html")
 
 
-def logout_view(request):
-    return render(request, "logout.html")
+@login_required(login_url="login")
+# log the user out
+def logout_view(user_request):
+    auth.logout(user_request)
+    return redirect("accounts:login")
