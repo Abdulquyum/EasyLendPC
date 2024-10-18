e53db0756c5d2d319de8ca4aa56bbbd89f606d88
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-10-18 08:07:07 +0100

    clean up

--- EasyLendPC/settings.py
+++ EasyLendPC/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-@61d+@2zb!dw7xbo@=95k2erfzb1758wvilr93_f8g9vk@1y@u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'App',
     'api',
     'authentication',
 ]
@@ -126,13 +125,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
